4b5010c4924836501589e588248140de1255f7a4
Søren Lorentzen <sorenlorentzen@outlook.com>
2025-09-26 21:32:45 +0200

    Added first API endpoint

--- Planner.Core/Entities/DatabaseContext.cs
+++ Planner.Core/Entities/DatabaseContext.cs
@@ -1,9 +1,19 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Sorenlorentzen.Invokable;
 
 namespace Planner.Core.Entities;
 
-public class DatabaseContext : DbContext
+public class DatabaseContext : DbContext, IDatabaseContext
 {
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    { }
+    
     public DbSet<Event> Events { get; set; }
+    
+    public DbSet<TEntity> GetDbSet<TEntity>() where TEntity : BaseEntity
+    {
+        return Set<TEntity>();
+    }
 }
